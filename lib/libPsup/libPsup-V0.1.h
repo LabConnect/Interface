9977c63ec35cca97f0406ead062729e613cae94c
Malte Sönnichsen <s.malte95@gmail.com>
2015-03-01 17:25:46 +0100

    add dev_handle.

--- lib/libPsup/libPsup-V0.1.h
+++ lib/libPsup/libPsup-V0.1.h
@@ -1,21 +1,40 @@
-#include "libPsup-V0.1.h"
-#include "../libUSB/libUSB-V0.1.h"
+#include <libusb.h>
 class Psup {
 public:
-  int   Init(int pid, int vid);
-  void  SetVoltage(float voltage);
-  void  SetCurrent(float current);
-  void  ReadVoltageBuffer(int &voltage, int &timestamp);
-  void  ReadCurrentBuffer(int &current, int &timestamp);
+   Psup(int vid, int pid);
+    ~Psup();
+  /* 0 = everything is fine
+   * 1 = device not found
+   * 2 = write failure
+   * 3 = read failure
+   */
+  int state = 0;
+  void Init(void);
+  void SetVoltage(float voltage);
+  void SetCurrent(float current);
+  void ReadVoltageBuffer(int &voltage, int &timestamp);
+  void ReadCurrentBuffer(int &current, int &timestamp);
 private:
-	libusb_device_handle *_dev_handle;
+  libusb_device_handle *_dev_handle;
+  libusb_context *_ctx = NULL;
+};
+
+Psup::Psup(unsigned int vid, unsigned int pid){
+  state = libusb_init(&ctx);
+  if (state >= 0){
+    _dev_handle = libusb_open_device_with_vid_pid(ctx,vid,pid);
+    if (_dev_handle == NULL) {
+      state = 1;
+    state = 0;
+  }
+
 }
 
-void Psup::Init(int vid, int pid){
-  _dev_handle = USB::Open(vid, pid);
+Psup::~Psup(){
+  libusb_exit(ctx);
 }
 
-void Psup::SetVoltage(float volgae){
+void Psup::SetVoltage(float voltage){
 
 }
 
